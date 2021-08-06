d9b619fc3e86f375c1099ccc4f74981e26c9e283
Anders Bech Mellson <mellson@icloud.com>
2021-08-06 09:39:35 +0200

    Preparing workshop

--- src/App.bs.js
+++ src/App.bs.js
@@ -6,7 +6,7 @@ function App(Props) {
   return React.createElement("div", {
               className: "bg-indigo-50 flex flex-col justify-center items-center h-screen"
             }, React.createElement("div", {
-                  className: "pb-8 text-5xl text-transparent font-extrabold bg-clip-text bg-gradient-to-br from-pink-400 to-indigo-400 animate-gradient drop-shadow-xl"
+                  className: "pb-8 drop-shadow-xl text-5xl text-transparent font-extrabold bg-clip-text bg-gradient-to-br from-pink-400 to-indigo-400 animate-gradient"
                 }, "ReScript Calculator"));
 }
 
